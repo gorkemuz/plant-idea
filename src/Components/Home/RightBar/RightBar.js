07cabd272cce96f09b05e68d50ab3853e32f3372
Eren ÇAY <ogealter@gmail.com>
2018-07-15 10:29:41 +0300

    Sağ menüdeki eksik key öğesini ekler

--- src/Components/Home/RightBar/RightBar.js
+++ src/Components/Home/RightBar/RightBar.js
@@ -5,7 +5,11 @@ const RightBar = ({ title }) => {
     return (
         <div className="Kategoriler">
             <p className="kategori-title">Popüler İçerikler</p>
-            {title.map(item => <p className="kategori">{item}</p>)}
+            {title.map((item, i) => (
+                <p key={i} className="kategori">
+                    {item}
+                </p>
+            ))}
         </div>
     );
 };
